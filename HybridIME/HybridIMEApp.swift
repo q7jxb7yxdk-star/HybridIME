3fd20e76adb47ea72a3860c50db4b426d86d7bcc
q7jxb7yxdk-star <q7jxb7yxdk@privaterelay.appleid.com>
2026-06-12 16:18:12 +0800

    Initial Commit

---
+++ HybridIME/HybridIMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridIMEApp.swift
+//  HybridIME
+//
+//  Created by Sunny Yu on 12/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct HybridIMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
